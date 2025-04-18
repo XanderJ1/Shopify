cd680ff992b38c3b44bacc9279ec9da1ac384498
XanderJ1 <bzakariyya6@gmail.com>
2025-04-18 10:38:18 +0000

    Add Orders Payment and Product

---
+++ src/views/Ordersview.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Orders from '@/components/Orders.vue';
+</script>
+
+<template>
+    <Orders />
+</template>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Product from '@/components/Product.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const productId = route.query.id
+console.log("Product", productId)
+</script>
+
+<template>
+    <Product :id= productId />
+</template>
